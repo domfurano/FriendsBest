6b184ef5d30cfa7440537505c76645ce1f1672b6
robobile <thefuzzyshark@gmail.com>
2015-11-23 23:06:37 -0700

    Update models.py

--- friends_best/models.py
+++ friends_best/models.py
@@ -1,34 +1,100 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 
 class User(models.Model):
-    token = models.CharField(max_length=256)
-    name = models.CharField(max_length=30)
+    userName = models.CharField(max_length=50, unique=True)
+    token = models.IntegerField(default=-1)
+
+    def __str__(self):
+        return self.userName
 
 
-class Friend(models.Model):
-    user = models.ForeignKey(User, related_name='user_set')
-    friend = models.ForeignKey(User, related_name='friend_set')
+class Friendship(models.Model):
+    userOne = models.ForeignKey(User, related_name='userOne_set')
+    userTwo = models.ForeignKey(User, related_name='userTwo_set')
+    muted = models.BooleanField(default=False)
 
-    class Meta:
-        unique_together = (('user', 'friend'), )
+    def save(self, *args, **kwargs):
+        if self.userOne != self.userTwo:
+            super(Friendship, self).save(*args, **kwargs)
+        else:
+            return "a user cannot be his/her own friend!"
+
+    def __str__(self):
+        return "%s -> %s " % (self.userOne, self.userTwo)
+
 
 class Query(models.Model):
-    user_id = models.ForeignKey(User)
-    timestamp = models.DateTimeField()
+    user = models.ForeignKey(User)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "%s at %s" % (self.user, self.timestamp)
 
 
 class Thing(models.Model):
-    type = models.IntegerField()
+    TEXT = 'TEXT'
+    THING_TYPE_CHOICES = (
+        (TEXT, 'Text'),
+    )
+    thingType = models.CharField(max_length=15, choices=THING_TYPE_CHOICES, default=TEXT)
+
+    def __str__(self):
+        return self.thingType
 
 
-class Recommend(models.Model):
-    thing_id = models.ForeignKey(Thing)
-    comments = models.CharField(max_length=1024)
-    timestamp = models.DateTimeField()
-    user_id = models.ForeignKey(User)
+class TextThing(models.Model):
+    thing = models.OneToOneField(Thing, primary_key=True)
+    content = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.content
 
 
-class Text(models.Model):
-    text = models.CharField(max_length=1000)
-    thing_id = models.ForeignKey(Thing)
+class Recommendation(models.Model):
+    thing = models.ForeignKey(Thing)
+    user = models.ForeignKey(User)
+    comments = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "user:%s, thing:%s, comments:%s" % (self.user, self.thing, self.comments)
+
+
+class Prompt(models.Model):
+    user = models.ForeignKey(User)
+    query = models.ForeignKey(Query)  # we can get the user's id from the query
+
+    def __str__(self):
+        return "forUser:%s, fromQuery:%s" % (self.user, self.query)
+
+
+class RecommendationTag(models.Model):
+    recommendation = models.ForeignKey(Recommendation)
+    tag = models.CharField(max_length=25)
+    lemma = models.CharField(max_length=25)
+
+    def __str__(self):
+        return "recommendation:%s, tag:%s)" % (self.recommendation, self.tag)
+
+
+class QueryTag(models.Model):
+    query = models.ForeignKey(Query)
+    tag = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "tag:%s, query:%s" % (self.tag, self.query)
+
+
+class Pin(models.Model):
+    thing = models.ForeignKey(Thing)
+    query = models.ForeignKey(Query)
+
+    def __str__(self):
+        return "thing:%s, query:%s" % (self.thing, self.query)
+
+
+
+
