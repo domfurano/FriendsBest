d3303387d73330f569ef9fe4121b1146f27bf6a9
domfurano <dominicfurano@gmail.com>
2016-03-22 17:48:56 -0600

    please work

--- ServerDjango/FriendsBest/settings.py
+++ ServerDjango/FriendsBest/settings.py
@@ -29,19 +29,20 @@ else:
 
 ALLOWED_HOSTS = ['*']
 
-ADMINS = [('Ray', 'ray@bluem33.com'),
+ADMINS = (('Ray', 'ray@bluem33.com'),
           ('Paul', 'paulhanson.exe@gmail.com'),
           ('Umair', 'unaveed@gmail.com'),
-          ('Dominic', 'dominicfurano@gmail.com')]
+          ('Dominic', 'dominicfurano@gmail.com'))
 
 if not DEBUG:
     EMAIL_USE_TLS = True
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_PASSWORD = os.environ['GmailPassword']
     EMAIL_HOST_USER = os.environ['GmailUser'] + '@gmail.com'
     EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+    SERVER_EMAIL = EMAIL_HOST_USER
 
 # Application definition
 
