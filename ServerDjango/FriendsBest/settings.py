449c3da4794da34e5d4169a650f0c00badc7cb8c
domfurano <dominicfurano@gmail.com>
2016-03-22 17:02:42 -0600

    hmm

--- ServerDjango/FriendsBest/settings.py
+++ ServerDjango/FriendsBest/settings.py
@@ -37,8 +37,8 @@ ADMINS = [('Ray', 'ray@bluem33.com'),
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['GmailPassword']
-EMAIL_HOST_USER = os.environ['GmailUser']
+EMAIL_HOST_PASSWORD = os.environ['GmailPassword'].encode()
+EMAIL_HOST_USER = os.environ['GmailUser'].encode()
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
