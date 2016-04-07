41e1e149d23c86346141de04bd11a371a723689a
Paul Hanson <thefuzzyshark@gmail.com>
2016-04-06 22:04:14 -0600

    debug6

--- ServerDjango/friends_best/services.py
+++ ServerDjango/friends_best/services.py
@@ -103,15 +103,18 @@ def getAllPrompts(user):
 
 def generateAnonymousPrompts(user):
     # get all queries made by users who are not friends with the user
-    queries = Query.objects.exclude(Q(user__userOne_set__userOne=user) | Q(user__userTwo_set__userTwo=user)).all()
+    #queries = Query.objects.exclude(Q(user__userOne_set__userTwo=user) | Q(user__userTwo_set__userOne=user)).all()
+    queries = Query.objects.exclude(user__userOne_set__userTwo=user)
     #queries = Query.objects.all()
     queryCount = queries.count()
-    #print("querycount: " + queryCount)
 
     # select random queries and generate prompts for them
+    randomIndexes = set()
     for x in range(0, 5):
         randomIndex = randint(0, queryCount - 1)
-        #print("randomindex: " + randomIndex)
+        randomIndexes.add(randomIndex)
+
+    for randomIndex in randomIndexes:
         p, created = Prompt.objects.get_or_create(user=user, query=Query.objects.all()[randomIndex])
 
 
