2199f9bda208295e5fdc651dcef7351e6b8c3c1e
Ray Phillips <ray@bluem33.com>
2016-04-12 18:11:27 -0600

    skipping validation

--- ServerDjango/friends_best/serializers.py
+++ ServerDjango/friends_best/serializers.py
@@ -273,10 +273,10 @@ class PinSerializer(serializers.ModelSerializer):
         return createPin(thingID, queryID)
         
     def validate(self, data):
-        if 'solutionid' not in data:
-            raise serializers.ValidationError('No solutionid provided')    
-        if 'queryid' not in data:
-            raise serializers.ValidationError('No queryid provided')
+#         if 'solutionid' not in data:
+#             raise serializers.ValidationError('No solutionid provided')    
+#         if 'queryid' not in data:
+#             raise serializers.ValidationError('No queryid provided')
         return data
     
     class Meta:
